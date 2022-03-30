744f06031df84b34d18476de5ffbc84e6088f2d9
leonitshaqiri <65737961+leonitshaqiri@users.noreply.github.com>
2022-03-30 18:28:30 +0200

    this push has one issue with : WEBPACK_IMPORTED_MODULE_1___default.a.createRoot

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -21,7 +21,7 @@ const Navigation = () => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={ctx.onLogout}>Logout</button>
+            <button onClick={ctx.onLogOut}>Logout</button>
           </li>
         )}
       </ul>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { AuthContextProvider } from "./store/auth-context";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
